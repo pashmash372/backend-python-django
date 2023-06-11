1d7b844fb43e3a9343d807bf42851e638c67010c
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-11 16:35:00 +0530

    setting up views
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

---
+++ 6_JANPython_06_pip_and_virtualenv_setup/main.py
@@ -0,0 +1,19 @@
+"""https://pypi.org/
+
+pip == maven == gradle
+
+library
+
+https://pip.pypa.io/en/stable/installation/
+
+PS C:\Users\a> where.exe python3
+C:\Users\a\AppData\Local\Microsoft\WindowsApps\python3.exe
+
+PS C:\Users\abhil> where.exe pip
+C:\Users\a\AppData\Local\Programs\Python\Python38\Scripts\pip.exe
+PS C:\Users\a>
+
+
+what is virtualenv
+
+how to maintain backward compability in python ? """
