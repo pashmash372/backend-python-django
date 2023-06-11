f880f4f7cb93e440e75acaece7b5b81133e07000
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-11 21:10:57 +0530

    lecture on models
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

---
+++ 10_JANDjango_04_Models/instaclone/users/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+# Create your views here.
+# index(request) request => HttpRequest
+def index(request):
+    message= f'{request.GET["my_favorite_color"]}'
+
+    my_favorite_color = request.GET.get('my_favorite_color')
+    if my_favorite_color:
+        message = f'Your favorite color is {my_favorite_color}'
+    else:
+        message = 'You did not tell me your favorite color.'
+
+    return HttpResponse(message)
