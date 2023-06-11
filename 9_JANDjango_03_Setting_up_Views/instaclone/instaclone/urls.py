d6bddba6bffd116be7f7d2abe4aa65d5a4817dd8
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-11 18:56:06 +0530

    setting up view 1
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- 9_JANDjango_03_Setting_up_Views/instaclone/instaclone/urls.py
+++ 9_JANDjango_03_Setting_up_Views/instaclone/instaclone/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # new
 
-
+# localhost:8000/users/...
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('users/', include('users.urls')),  # new
+    path('', include('users.urls')),  # new
+    #     BASE_URL + 'users/' + 'urls.py'
 ]
